a651f79456478f1d6fc921cf6228226d0dacaf47
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-11-13 20:02:40 +0100

    Update zad02/src/main/java/ug/ktrusilo/techut/zad02/Main.java

---
+++ zad02/src/main/java/ug/ktrusilo/techut/zad02/Main.java
@@ -0,0 +1,25 @@
+package ug.ktrusilo.techut.zad02;
+
+import java.sql.*;
+import java.text.ParseException;
+
+import ug.ktrusilo.techut.zad02.domain.Wardrobe;
+import ug.ktrusilo.techut.zad02.service.WardrobeService;
+
+public class Main {
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws SQLException, ParseException {
+
+			Wardrobe w1 = new Wardrobe("Marek", Double.parseDouble("200.85"), new Date(118, 12, 30), true);
+	    	Wardrobe w2 = new Wardrobe("Janina", Double.parseDouble("450.90"), new Date(115, 12, 30), false);
+	    	
+	        WardrobeService ws = new WardrobeService();
+	
+	        ws.addWardrobe(w1);
+	        ws.addWardrobe(w2);
+	        
+	        Wardrobe wardrobe = ws.getWardrobeById(2);
+	}
+
+}
