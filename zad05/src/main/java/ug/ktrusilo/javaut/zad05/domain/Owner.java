9af6f6a593289b6c2912a6af291e4b9b6ef2d0c2
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2018-12-23 19:40:26 +0100

    Create Owner.java

---
+++ zad05/src/main/java/ug/ktrusilo/javaut/zad05/domain/Owner.java
@@ -0,0 +1,49 @@
+package ug.ktrusilo.javaut.zad05.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Owner {
+	private long id;
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
