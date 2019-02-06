74ab93cde44ec69b1a1b3428fb3eaf6aad593862
krzysztoftrusilo <43780500+krzysztoftrusilo@users.noreply.github.com>
2019-02-06 02:09:17 +0100

    Create Evidence.java

---
+++ ProjektHibernate/src/main/java/ug/ktrusilo/javaut/zad04/domain/Evidence.java
@@ -0,0 +1,34 @@
+package ug.ktrusilo.javaut.zad04.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name="evidence.all",query="select w from Evidence w")
+})
+public class Evidence {
+	private long id;
+	private String code;
+	public Evidence() {
+		super();
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
